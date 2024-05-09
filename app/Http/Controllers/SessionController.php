7d6b0490fd214e033d7bd6c7d41964188d4c0a1c
MFirdausA <mfirdausakbar88@gmail.com>
2024-05-09 14:58:41 +0700

    controller and route change

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -1,33 +1,74 @@
 <?php
+
 namespace App\Http\Controllers;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
 class SessionController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        if($request->session()->has('user')) {
-            return $request->session()->get('user');
-        } else {
-            return 'Data tidak ditemukan pada session';
-        }
+        echo '<ul>';
+        echo '<li><a href=/buat-session>Buat Session</a></li>';
+        echo '<li><a href=/akses-session>Akses Session</a></li>';
+        echo '<li><a href=/hapus-session>hapus Session</a></li>';
+        echo '<li><a href=/flash-session>flash Session</a></li>';
+        echo '<ul>';
     }
-    public function store(Request $request)
+
+    public function buatSession()
     {
-        $request->session()->put('user','John Doe');
-        return "Data user sudah ditambah pada session";
+        session(['hakAkses' => 'admin', 'nama' => 'Joe']);
+        return "Session sudah dilihat";
     }
-    public function delete(Request $request)
+    public function aksesSession(Request $request )
     {
-        $request->session()->forget('user');
-        return "Data sudah dihapus pada session.";
+        //Menggunakan helper function
+        echo session('hakAkses');
+        echo session('nama');
+
+        echo '<hr>';
+
+        //dari request object
+        echo $request->session()->get('hakAkses');
+        echo $request->session()->get('nama');
+
+        echo '<hr>';
+
+        echo Session::get('hakAkses');
+        echo Session::get('nama');
+
+        echo '<hr>';
+
+        //Menggunakan faced Session
+        echo Session::get('hakAkses');
+        echo Session::get('nama');
     }
-    public function push(Request $request)
+    public function hapusSession(Request $request)
     {
-        if($request->session()->exists('user')) {
-            $request->session()->get('user.teams', 'developers');
-            return $request->session()->get('user');
-        } else {
-            return 'Data tidak ditemukan pada session';
-        }
+        //Menghapus semua session menggunakan helper function
+        session()->flush();
+
+        //Menghapus semua session menggunakan Request object
+        $request->session()->flush();
+
+        //Menghapus semua session menggunakan faced session\
+        Session::flush();
+
+        echo "semua Session sudah dihapus";
     }
+public function flashSession(Request $request)
+    {
+        //Membuat 1 flash session menggunakan helper function
+        session()->flash('hakAkses','admin');
+
+        //Membuat 1 flash session menggunakan Request object
+        // $request->session()->flash('hakAkses','admin');
+
+        //Membuat 1 flash session menggunakan facade Session
+        $request->session::flash('hakAkses','admin');
+
+    echo "Flash session hakAkses sudah dibuat";
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('session/put','App\Http\Controllers\SessionController@store');
-Route::get('session/get','App\Http\Controllers\SessionController@index');
-Route::get('session/push','App\Http\Controllers\SessionController@push');
-Route::get('session/del','App\Http\Controllers\SessionController@delete');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/' , [SessionController::class,'index']);
+Route::get('/buat-session' , [SessionController::class,'buatSession']);
+Route::get('/akses-session' , [SessionController::class,'aksesSession']);
+Route::get('/hapus-session' , [SessionController::class,'hapusSession']);
+Route::get('/flash-session' , [SessionController::class,'flashSession']);
